d5ddf1d27ac6004ba15ad54db76a51b314903b87
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2024-05-29 22:48:09 +0300

    create POST on api

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -1,11 +1,17 @@
 import connectMongoDB from "@/lib/mongodb";
 import { NextResponse } from "next/server";
-import Todo from '../../../models/todo';
+import Todo from '../../../models/todo'
 
 
 export async function POST (req){
-    const {title,description} = await req.json();
-    await connectMongoDB();
-    await Todo.create({title, description});
-    return NextResponse.json({message:"Todo created"},{status:201})
+    
+    try{
+        const {title,description} = await req.json();
+        await connectMongoDB();
+        await Todo.create({title, description});
+         return NextResponse.json({message:"Todo created"},{status:201})
+    }catch(e){
+         return NextResponse.json({message:`${e.message}`},{status:400})
+    }
+
 }
