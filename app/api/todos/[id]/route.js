f95c0cba3ef6e56122d57e7019bf40e7671e5a77
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2024-06-02 15:15:04 +0300

    added update and get by ID

--- app/api/todos/[id]/route.js
+++ app/api/todos/[id]/route.js
@@ -1,12 +1,20 @@
-import Todo from "@/components/Todo";
+import Todo from "../../../../models/todo";
 import connectMongoDB from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
 export async function PUT(req , {params}){
     const {id} = params;
-    const {newTitle, newDescription} = await req.json();
+    const {newTitle: title, newDescription: description} = await req.json();
     await connectMongoDB();
-    await Todo.findByIdAndUpdate(id, {newTitle, newDescription});
+    await Todo.findByIdAndUpdate (id, {title, description});
     return NextResponse.json({message:"todo updated"}, {status:200})
+
+}
+
+export async function GET(req, {params}){
+    const {id} = params;
+    await connectMongoDB();
+    const todo = await Todo.findOne({_id: id});
+    return NextResponse.json({todo}, {status:200})
 
 }
